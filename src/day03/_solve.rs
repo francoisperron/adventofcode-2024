9ab0a9fa6469e140c4e092b50b0bb21e65ffec84
Francois Perron <fperron@gmail.com>
2024-12-03 09:53:44 -0500

    Day 3 part 2

--- src/day03/_solve.rs
+++ src/day03/_solve.rs
@@ -18,5 +18,20 @@ mod tests {
         assert_eq!(instructions.sum(), 173731097);
     }
 
-    const EXAMPLE: &str = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+    #[test]
+    fn solves_part2_example() {
+        let instructions = Instructions::from(EXAMPLE);
+
+        assert_eq!(instructions.enabled_sum(), 48);
+    }
+
+    #[test]
+    fn solves_part2() {
+        let input = daily_input(3);
+        let instructions = Instructions::from(&input);
+
+        assert_eq!(instructions.enabled_sum(), 93729253);
+    }
+
+    const EXAMPLE: &str = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5)";
 }
--- src/day03/instructions.rs
+++ src/day03/instructions.rs
@@ -1,24 +1,67 @@
 use regex::Regex;
 
 pub struct Instructions {
-    multiplications: Vec<(usize, usize)>,
+    multiplications: Vec<(usize, usize, usize)>,
+    dos: Vec<usize>,
+    donts: Vec<usize>,
 }
 
 impl Instructions {
     pub fn from(input: &str) -> Instructions {
-        let regex = Regex::new(r"mul\((?<a>\d{1,3}),(?<b>\d{1,3})\)").unwrap();
+        let multiplications = Self::parse_multiplications(input);
+        let dos = Self::parse_dos(input);
+        let donts = Self::parse_donts(input);
         
-        let multiplications = regex
-            .captures_iter(input)
-            .map(|caps| (caps["a"].parse().unwrap(), caps["b"].parse().unwrap()))
-            .collect();
+        Instructions { multiplications, dos, donts }
+    }
 
-        Instructions { multiplications }
+    fn parse_multiplications(input: &str) -> Vec<(usize, usize, usize)> {
+        Regex::new(r"mul\((?<a>\d{1,3}),(?<b>\d{1,3})\)")
+            .unwrap()
+            .captures_iter(input)
+            .map(|caps| {
+                let index = caps.iter().next().unwrap().unwrap().start();
+                let a = caps["a"].parse().unwrap();
+                let b = caps["b"].parse().unwrap();
+                (index, a, b)
+            })
+            .collect()
+    }
+
+    fn parse_dos(input: &str) -> Vec<usize> {
+        Regex::new(r"do\(\)")
+            .unwrap()
+            .captures_iter(input)
+            .map(|caps| caps.iter().next().unwrap().unwrap().start())
+            .collect()
+    }
+
+    fn parse_donts(input: &str) -> Vec<usize> {
+        Regex::new(r"don't\(\)")
+            .unwrap()
+            .captures_iter(input)
+            .map(|caps| caps.iter().next().unwrap().unwrap().start())
+            .collect()
     }
 }
 
 impl Instructions {
     pub fn sum(&self) -> usize {
-        self.multiplications.iter().map(|(a, b)| a * b).sum()
+        self.multiplications
+            .iter()
+            .map(|(_, a, b)| a * b)
+            .sum()
+    }
+
+    pub fn enabled_sum(&self) -> usize {
+        self.multiplications
+            .iter()
+            .filter(|(index, _, _)| {
+                let enabled = self.dos.iter().rev().find(|i| *i < index).unwrap_or(&0);
+                let disabled = self.donts.iter().rev().find(|i| *i < index).unwrap_or(&0);
+                enabled >= disabled
+            })
+            .map(|(_, a, b)| a * b)
+            .sum()
     }
 }
