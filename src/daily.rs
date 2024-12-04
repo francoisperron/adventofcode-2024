c7ff976ad436a53281b90d9c451693088d17281a
Francois Perron <fperron@gmail.com>
2024-12-03 20:23:12 -0500

    Improve daily input error handling

--- src/daily.rs
+++ src/daily.rs
@@ -1,12 +1,21 @@
 use std::env;
 
 pub fn daily_input(day: u32) -> String {
-    let session = env::var("AOC_SESSION").unwrap();
+    match get_daily_input(day) {
+        Ok(input) => input,
+        Err(err) => panic!("Failed to get daily input: {}", err)
+    }
+}
 
-    let mut content = ureq::get(&format!("https://adventofcode.com/2024/day/{day}/input"))
+fn get_daily_input(day: u32) -> Result<String, Box<dyn std::error::Error>> {
+    let session = env::var("AOC_SESSION")?;
+
+    let response = ureq::get(&format!("https://adventofcode.com/2024/day/{}/input", day))
         .set("Cookie", &format!("session={}", session))
-        .call().unwrap()
-        .into_string().unwrap();
+        .call()?;
+
+    let mut content = response.into_string()?;
     content.pop();
-    content
+
+    Ok(content)
 }
